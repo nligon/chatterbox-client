66d13102d83b4365457740ef811f14157c391cce
nligon <nathanaelligon@gmail.com>
2016-05-02 15:25:36 -0700

    Set up file structure and implement alpha version of first method.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,11 @@
 // YOUR CODE HERE:
 
+// https://api.parse.com/1/classes/messages
+
+// **App ID:** dSNCwIniCXIY3dJz4KTuz9ROSUREvvCknGB11ReX
+
+// **REST API Key:** WsWpCFH7gaxcB2B0jVvyKPOcT5NVHriAulknx789
+
+// $.get('https://api.parse.com/1/classes/messages', )
+
+var app = {};
