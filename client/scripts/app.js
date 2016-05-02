eebe477ed3491e3fb0168c0b27d11ec33b67f4b1
nligon <nathanaelligon@gmail.com>
2016-05-02 16:23:49 -0700

    Render usernames and messages to document.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,3 +9,18 @@
 // $.get('https://api.parse.com/1/classes/messages', )
 
 var app = {};
+var results = $.get('https://api.parse.com/1/classes/messages', function() {
+
+  window.results2 = JSON.parse(results.responseText).results;
+  for (var i = 0; i < results2.length; i++) {
+    $('body').append(
+      `<div>
+         <p>Username: ${results2[i].username}</p>
+         <p>Message: ${results2[i].text}</p>
+       </div>`);
+  }
+});
+
+// results2 = JSON.parse(results.responseText)
+
+// setTimeout(function(){window.}, 1000);
