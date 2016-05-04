186d65b8b580ddb3176798f04ef94df201a1e799
nligon <nathanaelligon@gmail.com>
2016-05-03 17:01:40 -0700

    Implement chatroom change feature.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -13,6 +13,7 @@ app.init = function() {};
 app.send = function() {};
 var IDs = new Set();
 var latestRooms;
+var currentRoom = 'testingSpam';
 
 
 
@@ -20,6 +21,7 @@ var latestRooms;
 //make a new room dropdodwn list every refresh
 //have the array of rooms populate into the drop down
 // have the dropdown links reference a function that clears the chats and re-appends only those that contain the selected roomname
+
 
 // appends final set of rooms to dropdown
 var roomLister = function(roomSet) {
@@ -30,22 +32,77 @@ var roomLister = function(roomSet) {
   }
 };
 
-// Makes Set of roomnames from the parsed data array of message objects.
-var roomSetProducer = function(dataArray) {
-  var roomResults = new Set();
-  for (var i = 0; i < dataArray.length; i++) {
-    roomResults.add(dataArray[i].roomname);
-  }
-  latestRooms = roomResults;
-  return roomResults;
+var chooseRoom = function(roomName) {
+  currentRoom = roomName;
+  $('#chats').empty();
+  IDs = new Set();
+  updatePage(roomName);
+  return roomName;
 };
 
-var grabContentStartUpdate = function() {
+var updatePage = function(roomName) {
+  // console.log('updating!');
   var resultsObj = $.get('https://api.parse.com/1/classes/messages', function() {
     var dataArray = JSON.parse(resultsObj.responseText).results;
-    updater(dataArray);
+    updater(dataArray, roomName);
+  });
+
+  var updater = function(dataArray, roomName) {
+    // console.log(dataArray);
+    var allNewMessages = _.reject(dataArray, function(obj) {
+      return setContains(IDs, obj.objectId);
+    });
+    var filteredMessages = _.filter(allNewMessages, function(item) {
+      return item.roomname === roomName;
+    });
+    roomSetProducer(dataArray);
+    msgPlacer(filteredMessages);
+  };
+
+  // Makes Set of roomnames from the parsed data array of message objects.
+  var roomSetProducer = function(dataArray) {
+    var roomResults = new Set();
+    for (var i = 0; i < dataArray.length; i++) {
+      roomResults.add(dataArray[i].roomname);
+    }
+    latestRooms = roomResults;
+    return roomResults;
+  };
+
+  var msgPlacer = function(newMessages, roomName) {
+    for (var i = 0; i < newMessages.length; i++) {
+      $('#chats').prepend(
+        `<div>      
+         <p>Username: ${shieldXSS(newMessages[i].username)}</p>
+         <p>Message: ${shieldXSS(newMessages[i].text)}</p>
+         <p>Roomname: ${shieldXSS(newMessages[i].roomname)}</p>
+       </div>`);
+    }
+  };
+};
+
+
+var sendMsg = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify({
+      username: username,
+      text: document.getElementById('userInput').value,
+      roomname: currentRoom
+    }),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function(data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
 };
+
 
 var setContains = function(set, element) {
   if (set.has(element)) {
@@ -65,46 +122,4 @@ var shieldXSS = function(string = '') {
   return rejected.join('');
 };
 
-var msgPlacer = function(newMessages) {
-  for (var i = 0; i < newMessages.length; i++) {
-    $('#chats').prepend(
-      `<div>      
-         <p>Username: ${shieldXSS(newMessages[i].username)}</p>
-         <p>Message: ${shieldXSS(newMessages[i].text)}</p>
-         <p>Roomname: ${shieldXSS(newMessages[i].roomname)}</p>
-       </div>`);
-  }
-};
-
-var updater = function(dataArray) {
-  // console.log(dataArray);
-  var newMessages = _.reject(dataArray, function(obj) {
-    return setContains(IDs, obj.objectId);
-  });
-  roomSetProducer(dataArray);
-  msgPlacer(newMessages);
-};
-
-
-var sendMsg = function() {
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'POST',
-    data: JSON.stringify({
-      username: username,
-      text: document.getElementById('userInput').value,
-      roomname: "CatParty2016"
-    }),
-    contentType: 'application/json',
-    success: function(data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function(data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
-};
-
-setInterval(grabContentStartUpdate, 500);
+setInterval(function() { updatePage(currentRoom); }, 500);
